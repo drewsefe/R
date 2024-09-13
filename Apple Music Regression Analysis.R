c05bfd8b1fd553ac8d0b7ef1f26ae41eebb4d775
Andrew Barber <119080018+drewsefe@users.noreply.github.com>
2024-09-13 18:18:07 -0400

    Add files via upload

---
+++ Apple Music Regression Analysis.R
@@ -0,0 +1,30 @@
+# connnecting to SQL Server
+con <- odbcConnect('portfolio', uid = 'sa', pwd = 'Moon12281997_')
+music <- sqlQuery(con, 'select * from music')
+
+# statistical models
+mean(music$`Play Duration Minutes`)
+mode <- names(table(music$`Play Duration Minutes`))[which.max(table(music$`Play Duration Minutes`))]
+mode
+median(music$`Play Duration Minutes`)
+var(music$`Play Duration Minutes`)
+sd(music$`Play Duration Minutes`)
+
+
+mins <- sqlQuery(con, 'select [Year],[Month],SUM([Play Duration Minutes]) as [Total Minutes],
+RANK() over(partition by [Year], [Month] order by SUM([Play Duration Minutes]) desc) as [Rank]
+from music
+group by [Year],[Month]
+order by [Total Minutes] desc')
+
+
+#Quick visualizations with regression models
+ggplot(mins, aes(Year, `Total Minutes`, color = Month)) +
+  geom_point()
+
+ggplot(mins, aes(Year, `Total Minutes`)) + 
+  geom_point() + 
+  geom_smooth(method = 'loess') +
+  facet_wrap(~ Month)
+
+cor(mins$`Total Minutes`,mins$Year)
