956db35dfeb637a48a29c6615dc9516e0176e588
Andrew Barber <119080018+drewsefe@users.noreply.github.com>
2024-09-23 13:24:35 -0400

    Apple Music Regression Analysis.R

--- Apple Music Regression Analysis.R
+++ Apple Music Regression Analysis.R
@@ -4,12 +4,26 @@ music <- sqlQuery(con, 'select * from music')
 
 # statistical models
 mean(music$`Play Duration Minutes`)
+
 mode <- names(table(music$`Play Duration Minutes`))[which.max(table(music$`Play Duration Minutes`))]
+
 mode
+
 median(music$`Play Duration Minutes`)
+
 var(music$`Play Duration Minutes`)
+
 sd(music$`Play Duration Minutes`)
 
+lm(Year ~ `Total Minutes`, data = mins)
+
+#Probability models
+pnorm(5000, mean = 4431.102, sd = 1101.64)
+
+pnorm(5000, mean = 4431.102, sd = 1101.64, lower.tail = FALSE)
+
+#Correlation models
+cor(mins$`Total Minutes`,mins$Year)
 
 mins <- sqlQuery(con, 'select [Year],[Month],SUM([Play Duration Minutes]) as [Total Minutes],
 RANK() over(partition by [Year], [Month] order by SUM([Play Duration Minutes]) desc) as [Rank]
@@ -27,4 +41,3 @@ ggplot(mins, aes(Year, `Total Minutes`)) +
   geom_smooth(method = 'loess') +
   facet_wrap(~ Month)
 
-cor(mins$`Total Minutes`,mins$Year)
